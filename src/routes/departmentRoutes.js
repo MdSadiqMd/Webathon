c218729ac4e075176d255e13a73a4aaf58ba4f1f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-06 23:11:57 +0530

    reafactor added allotment table

--- src/routes/departmentRoutes.js
+++ src/routes/departmentRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const departmentController = require('../controllers/departmentController');
 
-// Define routes for department incharge-related actions
 router.get('/viewSchedules', departmentController.viewSchedules);
 router.post('/requestAdjustment', departmentController.requestAdjustment);
 router.get('/viewAdjustmentStatus', departmentController.viewAdjustmentStatus);
--- src/routes/facultyRoutes.js
+++ src/routes/facultyRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const facultyController = require('../controllers/facultyController');
 
-// Define routes for faculty-related actions
 router.get('/viewAssignedDuties', facultyController.viewAssignedDuties);
 router.post('/requestAdjustment', facultyController.requestAdjustment);
 router.post('/reportAbsentees', facultyController.reportAbsentees);
