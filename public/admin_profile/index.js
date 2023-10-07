f9c572cd37a194a228061672d8cc731360d5867f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2023-10-07 10:20:57 +0530

    refactor added admin profile

--- public/admin_profile/index.js
+++ public/admin_profile/index.js
@@ -142,3 +142,111 @@ if (roomsListCount!=0){
         create_room_ele(eachRoom)
     }
 }
+
+let absBtn1=document.getElementById("abs-btn1")
+let absBtn2=document.getElementById("abs-btn2")
+let absBtn3=document.getElementById("abs-btn3")
+let absBtn4=document.getElementById("abs-btn4")
+
+let CourseName=document.getElementById("CourseName");
+let ExamDate=document.getElementById("ExamDate");
+let examType=document.getElementById("examType")
+let markAbsenceBtn=document.getElementById("markAbsenceBtn")
+let listOfSlected=[]
+
+absBtn1.addEventListener("click",()=>{
+    absBtn1.classList.toggle("numClicked")
+    markAbsenceBtn.addEventListener("click",()=>{
+        if (CourseName.value==="" || ExamDate.value===null || examType.value===null){
+            alert("Please fill in all fields before adding a request.");
+        }else{
+            listOfSlected.push({student_number:absBtn1.textContent})
+        }
+    })
+})
+markAbsenceBtn.addEventListener("click",()=>{
+    if (CourseName.value==="" || ExamDate.value===null || examType.value===null){
+        alert("Please fill in all fields before adding a request.");
+    }})
+
+// document.getElementsByClassName("abs-btn").addEventListener("click", function () {
+//     console.log("clicked")
+// })
+//     const courseName = document.getElementById("CourseName").value;
+//     const examDate = document.getElementById("ExamDate").value;
+//     const examType = document.getElementById("examType").value;
+
+//     const selectedStudentIDs = [];
+//     document.querySelectorAll('.attendence > button.selected').forEach(button => {
+//         const studentID = button.textContent.trim();
+//         selectedStudentIDs.push(studentID);
+//     });
+
+//     if (courseName && examDate && examType && selectedStudentIDs.length > 0) {
+//         const table = document.querySelector("table");
+//         const tbody = table.querySelector("tbody");
+
+//         selectedStudentIDs.forEach(studentID => {
+//             const row = document.createElement("tr");
+//             row.innerHTML = `
+//                 <td>${studentID}</td>
+//                 <td>${courseName}</td>
+//                 <td>${examDate}</td>
+//                 <td>${examType}</td>
+//                 <td><button class="deleteBtn">Delete</button></td>
+//             `;
+//             document.getElementById("absenceTableBody").appendChild(row);
+//         });
+
+//         document.getElementById("CourseName").value = "";
+//         document.getElementById("ExamDate").value = "";
+//         document.getElementById("examType").value = "SEM-END";
+//         document.getElementById("errorMsg").textContent = "";
+//     } else {
+//         if (selectedStudentIDs.length === 0) {
+//             document.getElementById("errorMsg").textContent = "Please select one or more students before marking absence.";
+//         } else {
+//             document.getElementById("errorMsg").textContent = "Please fill in all fields before marking absence.";
+//         }
+//     }
+// });
+
+// document.getElementById("absenceTableBody").addEventListener("click", function (e) {
+//     if (e.target.classList.contains("deleteBtn")) {
+//         e.target.closest("tr").remove();
+//     }
+// });
+
+// document.getElementById("addRequestBtn").addEventListener("click", function () {
+//     const requestDate = document.getElementById("requestDate").value;
+//     const requestRoom = document.getElementById("requestRoom").value;
+//     const requestType = document.getElementById("requestType").value;
+//     const requestTimings = document.getElementById("requestTimings").value;
+
+//     if (requestDate && requestRoom && requestType && requestTimings) {
+//         const table = document.querySelector(".requestTable table");
+//         const tbody = table.querySelector("tbody");
+
+//         const row = document.createElement("tr");
+//         row.innerHTML = `
+//             <td>${requestDate}</td>
+//             <td>${requestRoom}</td>
+//             <td>${requestType}</td>
+//             <td>${requestTimings}</td>
+//             <td>Pending</td>
+//         `;
+//         tbody.appendChild(row);
+
+//         document.getElementById("requestDate").value = "";
+//         document.getElementById("requestRoom").value = "";
+//         document.getElementById("requestType").value = "Swap";
+//         document.getElementById("requestTimings").value = "";
+//     } else {
+//         alert("Please fill in all fields before adding a request.");
+//     }
+// });
+
+// document.querySelectorAll('.attendence > button').forEach(button => {
+//     button.addEventListener("click", () => {
+//         button.classList.toggle("selected");
+//     });
